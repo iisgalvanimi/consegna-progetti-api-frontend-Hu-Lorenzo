a3ad07187b2fa12f5952798fccc69789d0ec1f56
Lorenzo Hu <hu.lorenzo@iisgalvanimi.edu.it>
2024-11-11 13:11:50 +0000

    fatto il routing

---
+++ Animali/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FruttaComponent } from './frutta/frutta.component';
+import { AnimaliComponent } from './animali/animali.component';
+
+const routes: Routes = [
+  { path: 'animali', component: AnimaliComponent},
+  { path: 'frutta', component: FruttaComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
