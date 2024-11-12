cb34f7837dd6f1bcafdf2409de779969001b112e
Lorenzo Hu <hu.lorenzo@iisgalvanimi.edu.it>
2024-11-12 12:55:31 +0000

    15.2 fatto

--- chiamata_http/src/app/foo/foo.component.ts
+++ chiamata_http/src/app/foo/foo.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {Foo} from './foo.model';
 
 @Component({
+
  selector: 'app-foo',
  templateUrl: './foo.component.html',
  styleUrls: ['./foo.component.css']
 })
 export class FooComponent {
-   data!: Object; //Il ‘!’ serve a creare variabili non inizializzate
+  fooData!: Foo[];
+  data !: Object;
    loading: boolean=false;
    o! :Observable<Object>;
+   oFoo! : Observable<Foo[]>;
    constructor(public http: HttpClient) {}
    makeRequest(): void {
      console.log("here");
      this.loading = true;
-     this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
+     this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/2');
      this.o.subscribe(this.getData);
    }
    getData = (d : Object) =>
@@ -29,7 +33,7 @@ export class FooComponent {
    makeCompactRequest(): void {
      this.loading = true;
      this.http
-       .get('https://jsonplaceholder.typicode.com/posts/1')
+       .get('https://jsonplaceholder.typicode.com/posts/2')
        .subscribe(newData => {
        this.data = newData;
        this.loading = false;
@@ -42,7 +46,7 @@ export class FooComponent {
             JSON.stringify({
               body: 'bar',
               title: 'foo',
-              userId: 1
+              userId: 2
             })
           )
           .subscribe(data => {
@@ -50,5 +54,15 @@ export class FooComponent {
             this.loading = false;
           });
       }
-     
+      makeTypedRequest() : void
+      {
+        //oFoo : Observable<Foo[]>; va dichiarato tra gli attributi della classe 
+        this.oFoo = this.http.get<Foo[]>('https://jsonplaceholder.typicode.com/posts');
+        this.oFoo.subscribe(data => {this.fooData = data;});
+      }
+    
+      ngOnInit() {
+      }
 }
+
+
